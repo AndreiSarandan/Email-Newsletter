77c2909c00751e5215f65793a2c6cfb60a6bc5aa
Andrei Sarandan <80057863+AndreiSarandan@users.noreply.github.com>
2022-09-25 11:01:26 +0300

    Update Newsletter.py

--- Newsletter.py
+++ Newsletter.py
@@ -3,26 +3,26 @@ import smtplib
 
 def send_email(user, pwd, recipient, subject, body):
     FROM = user
-    TO = recipient if isinstance(recipient, list) else [recipient]
+    TO = recipient if isinstance(recipient, list) else [recipient] #single or multiple addresses
     SUBJECT = subject
     CTEXT = body
 
     message = """From: %s\nTo: %s\nSubject: %s\n\n%s
     """ % (FROM, ", ".join(TO), SUBJECT, CTEXT)
     try:
-        server = smtplib.SMTP('smtp.gmail.com:587')
-        server.ehlo()
+        server = smtplib.SMTP('smtp.gmail.com:587') #initialize connection to server
+        server.ehlo() 
         server.starttls()
-        server.login(user, pwd)
-        server.sendmail(FROM, TO, message)
-        server.close()
+        server.login(user, pwd) #google account log-in
+        server.sendmail(FROM, TO, message)#send email
+        server.close() #close connection
         print('successfully sent the mail')
     except:
         print("failed to send mail")
 
 
 user = 'newstester778@gmail.com'
-pwd = 'dtfhspgkytcvyxju'
+pwd = 'dtfhspgkytcvyxju' #
 subject = 'Test email'
 with open('message.txt', 'r') as e:
     text = e.readlines()
